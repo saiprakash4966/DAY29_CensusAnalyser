b4cb29eecacd5d24e347f867f0633314322206b4
Your Name <you@example.com>
2022-03-25 19:36:52 +0530

    Information from CSV file

--- main/java/com/bl/CsvToBean.java
+++
@@ -1,9 +0,0 @@
-package com.bl;
-
-import java.util.Iterator;
-
-public interface CsvToBean<T> {
-
-	Iterator<IndiaCensusCSV> iterator();
-
-}
--- main/java/com/opencsv/bean/CsvBindByName.java
+++
@@ -1,9 +0,0 @@
-package com.opencsv.bean;
-
-public @interface CsvBindByName {
-
-	String column();
-
-	boolean required();
-
-}
